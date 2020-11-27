553d3f79c0f99a86d8075604249f208215ece967
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-27 22:19:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(string)
   list_string = string.split(" ")
   print list_string
+  list_string.each do |word|
+    puts word
+  end
 end
 
 reverse_each_word("Danielle loves icecream")
