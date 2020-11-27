39b44f46f44657783d4443da00730c9e515dd05f
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-27 22:34:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(string)
   list_string = string.split(" ")
- # print list_string
   count = 0
   while count < list_string.length do
     list_string[count] = list_string[count].reverse
     count += 1
   end
+  
   string = list_string.join(" ")
   string
   #list_string.collect do {|word| word.reverse}
