27d55efc8c94044662fbc0a716c78c0a5dbbbb39
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-27 22:22:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   list_string = string.split(" ")
   print list_string
-  list_string.collect do {|word|
+  list_string.collect do {|word| word.reverse}
     puts word
   end
 end
